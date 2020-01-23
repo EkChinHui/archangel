2030b13d2e1442331c149a842f579406ae590fb3
Sam <46836091+Sam-limyr@users.noreply.github.com>
2020-01-23 16:06:26 +0800

    Substitute commas in model for semicolons.
    Commas in model complicate exporting to CSV. Commas in model fields are now replaced by semicolons.

--- models.py
+++ models.py
@@ -10,17 +10,20 @@ class Player:
             return self.separate_args_with_commas(self.name, self.fbname, self.floor, self.room_number, self.contact_number, self.gender, self.year, self.gender_pref, self.faculty, self.interests)
 
         def __init__(self, **kwargs):
-                self.name = kwargs.get('name')
-                self.fbname = kwargs.get('fbname')
-                self.floor = int(kwargs.get('floor'))
-                self.room_number = kwargs.get('room_number')
-                self.contact_number = kwargs.get('contact_number')
-                self.gender = kwargs.get('gender')
-                self.year = int(kwargs.get('year'))
-                self.gender_pref = kwargs.get('gender_pref')
-                self.faculty = kwargs.get('faculty')
-                self.interests = kwargs.get('interests')
-
+                self.name = kwargs.get('name').replace(",", ";")
+                self.fbname = kwargs.get('fbname').replace(",", ";")
+                try:
+                        self.floor = int(kwargs.get('floor'))
+                except ValueError:
+                        self.floor = 0
+                self.room_number = kwargs.get('room_number').replace(",", ";")
+                self.contact_number = kwargs.get('contact_number').replace(",", ";")
+                self.gender = kwargs.get('gender').replace(",", ";")
+                self.year = kwargs.get('year').replace(",", ";")
+                self.gender_pref = kwargs.get('gender_pref').replace(",", ";")
+                self.faculty = kwargs.get('faculty').replace(",", ";")
+                self.interests = kwargs.get('interests').replace(",", ";")                
+                
         def __repr__(self):
                 return str(self.name)
 
